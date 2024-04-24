0a9a301722cb29b7d8d2a5ce6c4c4e66543c16be
Mauro <maurodeveloper86@gmail.com>
2024-04-24 17:47:47 -0300

    Init

---
+++ resources/js/Pages/Dashboard.jsx
@@ -0,0 +1,20 @@
+import TableList from '@/Components/Table';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
+
+export default function Dashboard({ auth }) {
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Products</h2>}
+        >
+            <Head title="Products" />
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <TableList/>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
